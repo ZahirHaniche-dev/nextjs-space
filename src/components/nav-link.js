9f94bd9d45bfe12ca955b5307db021cac2ae631e
Haniche Zahir <zahir.haniche@inetum.com>
2024-09-15 19:01:34 +0200

    Add Framer Motion

--- src/components/nav-link.js
+++ src/components/nav-link.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { motion } from 'framer-motion';
 
 export default function NavLink({href, children}) {
 
@@ -12,7 +13,18 @@ const path = usePathname();
     <>
       <Link href={href} 
             className={`${path.startsWith(href) ? 'text-white text-lg' : 'text-gray-500 text-lg'} hover:text-white`}>
-        {children}
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          style={{ position: 'relative', display: 'inline-block' }}
+        >
+          {children}
+          <motion.div
+            style={{ position: 'absolute', bottom: 0, left: 0, height: '2px', backgroundColor: 'white' }}
+            initial={{ width: 0 }}
+            whileHover={{ width: '100%' }}
+            transition={{ duration: 0.3 }}
+          />
+        </motion.div>
       </Link>
     </>
   )
