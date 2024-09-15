690c96009f5bb1ef4aaec2a827ca49a27737bbce
Haniche Zahir <zahir.haniche@inetum.com>
2024-09-15 20:08:31 +0200

    Add page Destination

---
+++ src/app/destination/page.js
@@ -0,0 +1,9 @@
+import '../../app/globals.css';
+
+export default function Destination() {
+  return (
+    <main className={`main bg-destination-mobile md:bg-destination-tablet lg:bg-destination-desktop px-32`}>
+
+    </main>
+  )
+}
